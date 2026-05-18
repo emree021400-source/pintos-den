ef2dd0c8ce2bd5482b849e07022c755fcffe9ef6
emree021400-source <emree021400@gmail.com>
2026-05-18 15:41:56 +0300

    Refactor timer_sleep and add thread_check_sleep

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -89,11 +89,16 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
+ void
+timer_sleep (int64_t ticks) 
+{
+ void
+timer_sleep (int64_t ticks) 
+{
   int64_t start = timer_ticks ();
+  if (ticks <= 0) return;
+  thread_sleep (start + ticks);
 
-  ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -172,6 +177,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
   thread_tick ();
+  thread_check_sleep (ticks); /* EKLENEN SATIR BURASI */
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
